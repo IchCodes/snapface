2c4edbfe03862a647ed08c020089e065d82dd855
IchCodes <hichame.sraidi@gmail.com>
2024-11-19 14:30:51 +0100

    array of facesnaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,34 +10,35 @@ import {FaceSnap} from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Junayd mon fils ',
-      'Mon fils ',
-      'https://cdn.pixabay.com/photo/2016/05/17/22/16/baby-1399332_640.jpg',
-      new Date(),
-      8
-    );
-    this.myOtherSnap = new FaceSnap(
+
+    this.faceSnaps = [
+      new FaceSnap(
+        'Junayd mon fils ',
+        'Mon fils ',
+        'https://cdn.pixabay.com/photo/2016/05/17/22/16/baby-1399332_640.jpg',
+        new Date(),
+        8
+      ),new FaceSnap(
       'Vue de la montagne',
       'Un superbe paysage montagneux',
       'https://cdn.pixabay.com/photo/2021/10/11/18/58/lake-6701636_1280.jpg',
       new Date(),
       15
-    );
-
-    this.myLastSnap = new FaceSnap(
+    ),new FaceSnap(
       'Coucher de soleil',
       'Un magnifique coucher de soleil sur la plage',
       'https://cdn.pixabay.com/photo/2023/02/02/10/02/sunset-7762468_640.jpg',
       new Date(),
       22
-    );
+    )
+    ];
 
-    this.myOtherSnap.setLocation('Montagne');
+    this.faceSnaps[1].setLocation('Montagne');
   }
 }
